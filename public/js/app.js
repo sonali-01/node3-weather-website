ef0c3ac9a6161b77c9f9eccf909af3ffb8ac66e5
sonali-01 <sonali.sk@robosoftin.com>
2022-09-06 14:48:21 +0530

    Setup app for Horoku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit',(e)=>{
     const location=inputElement.value
     messageOne.textContent='Loading.....'
     messageTwo.textContent=''
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+    fetch(`/weather?address=${location}`).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
                 console.log("error:",data.error)
